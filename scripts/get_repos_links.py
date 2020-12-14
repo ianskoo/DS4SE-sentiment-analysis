9ef555cb1cb1a2fd0a1de96826aa682bc640c6ef
ianskoo <christian.skorski@outlook.com>
2020-12-14 21:36:34 +0100

    link-making script enhanced with try/catch and token

--- scripts/get_repos_links.py
+++ scripts/get_repos_links.py
@@ -5,21 +5,34 @@ nr_proj = 10
 
 # Define and modify call to GitHub API
 link = "\
-    q=stars:>=10000\
+    q=stars:>=1000\
     &updated:<2020-01-01\
     &per_page=100\
     &sort=stars\
-    &order=desc\
+    &order=asc\
     &page=" # Needed at the end for page number
 
-with open("links.txt", 'w+') as f:
+with open("./data/links_medium.txt", 'w+') as f:
 
     # Request 100 projects per page ("nr_proj" nr of pages)
     for p in range(nr_proj):
-        r = requests.get(f"https://api.github.com/search/repositories?{link}{p}")
-        # headers = r.headers
-        data = r.json()
+        try:
+            r = requests.get(
+                f"https://api.github.com/search/repositories?{link}{p}",
+                headers={'Accept': 'application/json', 'Authorization': 'token 5a26950162202d524bcd2e14307f9ab0505f7387'}
+                )
 
-        # Write links to file
-        for i in range(len(data['items'])):
-            f.write(data['items'][i]['html_url'] + '\n')
+            if isinstance(r, list):
+                data = r.json()
+
+                # Write links to file
+                for i in range(len(data['items'])):
+                    f.write(data['items'][i]['html_url'] + '\n')
+
+            else:
+                print(r)
+                break
+
+        except Exception as e:
+            print("Error: ", e)
+            break
