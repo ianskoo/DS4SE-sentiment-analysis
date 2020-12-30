d34891f68c9c6db006570e11adc8b76f9c56265b
ianskoo <christian.skorski@outlook.com>
2020-12-30 13:00:51 +0100

    fixed mistake in year range selection for API; fixed problem with API page iteration; new dataset of projects w/ 300 stars

--- scripts/makelogs_API.py
+++ scripts/makelogs_API.py
@@ -6,7 +6,7 @@ import numpy as np
 # ---------------------------------------
 # Parameters for the API request:
 per_page = 100
-params = f"?since:2020-01-01T00:00:00Z&per_page={per_page}&page="
+params = f"?since=2020-01-01T00:00:00Z&per_page={per_page}&page="
 # ---------------------------------------
 
 # Open file with links to repositories
@@ -18,21 +18,25 @@ with open('./data/links_small.txt') as links:
 
 # Insert token in 'Authorization'
 # Returns an API response for a repository at index repo_index
-def make_request(repo_index, per_page, params):
+def make_request(repo_index, per_page, page_nr, params):
     
-    commits_info = requests.get(f"https://api.github.com/repos/{repos[repo_index]}/commits{params}",
+    commits_info = requests.get(f"https://api.github.com/repos/{repos[repo_index]}/commits{params}{page_nr}",
                                 headers={'Accept': 'application/json', 'Authorization': 'token 5a26950162202d524bcd2e14307f9ab0505f7387'})
 
-    if commits_info.headers['X-RateLimit-Remaining'] == '1':
+    # Sleep while API limit reached
+    while commits_info.headers['X-RateLimit-Remaining'] == '0':
         print("Waiting 60s to avoid API rate limit...")
         time.sleep(60)
+        # Try again
+        commits_info = requests.get(f"https://api.github.com/repos/{repos[repo_index]}/commits{params}{page_nr}",
+                                headers={'Accept': 'application/json', 'Authorization': 'token 5a26950162202d524bcd2e14307f9ab0505f7387'})
 
     return commits_info.json()
 
 
 # Function to save the downloaded data
 def save_data(array):
-    print("Saving data, this could take a moment...", end="\r")
+    # print("Saving data...", end="\r")
     # np.save('./data/magic_array', array)
 
     with open('./data/repos_commits2.csv', 'w', newline='', encoding='utf-8') as file:
@@ -48,12 +52,16 @@ comm_cnt = 0
 stop = 0
 
 for i in range(len(repos)):
+
+    # Initialize page nr for iteration over API resulting pages
+    page = 1
+
     while True:
         print(f"{i}/1000 repos done -- {comm_cnt} messages retrieved", end="\r")
 
         # Try requesting messages for given repo, if an error occurs save everything.
         try:
-            commits = make_request(i, per_page, params)
+            commits = make_request(i, per_page, page, params)
             if isinstance(commits, list):
                 req_cnt += 1
                 # Iterate over all commits of a single project
@@ -67,15 +75,19 @@ for i in range(len(repos)):
 
                         arr.append([i, date, message, person])
 
+                # Increase page counter to continue iteration of messages on next API page
+                page += 1
+
             # Condition to break out of the while loop (finish curr repo)
             if len(commits) < per_page:
                 break
                         
         # Save data if download is interrupted
         except Exception as e:
-            # save_data(arr)
-            print(f"Error: {e} \nAPI rate limit reached! Downloaded repositories: %d" %req_cnt)
-    
+            save_data(arr)
+            print(f"Error: {e} \n. Downloaded repositories: %d" %req_cnt)
+            break
+
     # Save to csv after each repo
     save_data(arr)
 
